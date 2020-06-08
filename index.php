0707a813c132b7544162271bed8448b16536418b
Romadhan Prasetyo <romadhanedy@gmail.com>
2020-06-08 10:59:20 +0700

    Dytictactoe JSGame

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+include('dytictactoe-dygames.dev.html');
+?>
---
+++ js/Custom.js
@@ -0,0 +1,9 @@
+let cpDiv = document.createElement('div');
+cpDiv.classList.add('cpright');
+body.appendChild(cpDiv);
+
+let cpText = document.createElement('span');
+cpText.classList.add('pcpright');
+$('.cpright').appendChild(cpText);
+
+$('.pcpright').innerText = "Copyright © 2020 | Dygames.dev";
